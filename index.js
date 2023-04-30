4099951d6e1586c959d77e92264e3d8a911fca2f
Dane Kargis <dane.kargis@gmail.com>
2023-04-30 17:48:45 -0600

    working on a function to generate the svg file

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-const Shapes = require('./shapes');
+const Shapes = require('./lib/shapes');
 const fs = require('fs');
 const path = require('path');
 const inqure = require('inquirer');
-const { choices } = require('yargs');
+const { type } = require('os');
 
 const shape = new Shapes();
 
-const questions = [
+const questions = 
+    .prompt([
+    
     {
         type: 'list',
         name: 'shape',
         message: 'What shape would you like to draw?',
-        choices: ['Triangle', 'Square', 'Circle']
+        choices: [
+            'Triangle', 
+            'Square', 
+            'Circle']
     },
     {
         type: 'input',
         name: 'color',
         message: 'What color would you like to draw with?'
-        choices: [
-            'red', 
-            'blue', 
-            'green']
+        validate: function (value) {
+            // Check if the input is a valid color name or number
+            if (/^[a-zA-Z]+$/.test(value) || /^\d+$/.test(value)) {
+              return true;
+            }
+            return 'Please enter a valid color name or number';
+            
+            }
     },
     {
         type: 'input',
         name: 'logo',
         message: 'Instert a three letter logo'
     }
-];
+]);
